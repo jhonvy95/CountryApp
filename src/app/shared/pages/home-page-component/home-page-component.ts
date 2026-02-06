25741b57e80a322aa1a9af6723cd6a9d96899d98
jhon <jyate@smartsoft.la>
2026-02-05 22:53:06 -0500

    add by-capital and home page components with initial templates

---
+++ src/app/shared/pages/home-page-component/home-page-component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
+
+@Component({
+  selector: 'app-home-page-component',
+  imports: [
+    RouterLink
+  ],
+  templateUrl: './home-page-component.html',
+  styles: ``,
+})
+export class HomePageComponent {
+
+}
