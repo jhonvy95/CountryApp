2c3589c4def1b53c2316717df3592344a88c03de
jhon <jyate@smartsoft.la>
2026-02-17 23:38:27 -0500

    add country list component with initial template

---
+++ src/app/country/components/country-list-component/country-list-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'country-list-component',
+  imports: [],
+  templateUrl: './country-list-component.html',
+  styles: ``,
+})
+export class CountryListComponent {
+
+}
